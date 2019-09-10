3aa6c6c12458f1b9074f07d3da412779b56679f8
margauxgibert15 <margaux.gibert15@hotmail.fr>
2019-09-10 15:39:43 +0200

    Ohhlala css addict

--- routes/auth.js
+++ routes/auth.js
@@ -1,42 +1,32 @@
-const express = require("express")
+const express = require("express");
 const router = new express.Router();
-
 
 //SIGNUP
 
 router.get("/signup", (req, res) => {
+  res.render("auth/signup_choice");
+});
 
-    res.render("auth/signup_choice")
+router.get("/musician", (req, res) => {
+  res.render("auth/musician_chosen");
+});
 
-})
-
-router.get("/signup/1", (req, res) => {
-    
-    res.render("auth/signup_choice")
-
-})
+router.get("/listener", (req, res) => {
+  res.render("auth/listener_chosen");
+});
 
 router.get("/signup/2", (req, res) => {
-    
-    res.render("auth/signup_chosen")
-
-})
+  res.render("auth/signup_chosen");
+});
 
 router.get("/signup/3", (req, res) => {
-
-    res.render("auth/signup_final")
-
-})
+  res.render("auth/signup_final");
+});
 
 //CONNECT
 
 router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
 
-    res.render("auth/login")
-
-})
-    
-    
-
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const hbs = require("hbs")
-const path = require("path")
-const app = express()
-const artistModel = require("./models/artist.js")
-const mongoose = require("mongoose")
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
+const artistModel = require("./models/artist.js");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -17,41 +17,25 @@ let auth = require("./routes/auth.js");
 let musician = require("./routes/musician.js");
 let user = require("./routes/user.js");
 
-let pass = process.env.DB_PASS
-let pseudo = process.env.DB_USER
+let pass = process.env.DB_PASS;
+let pseudo = process.env.DB_USER;
 
 const uri = `mongodb+srv://${pseudo}:${pass}@cluster0-rcuew.gcp.mongodb.net/fiddle?retryWrites=true&w=majority`;
 
 mongoose
-    .connect(uri, { useNewUrlParser: true })
-    .then(() => console.log("connected to DataBase"))
-    .catch(err => console.log(err))
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("connected to DataBase"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
-  res.render("user/index", { test: "coucou" });
+  artistModel.find().then(dbres => {
+    res.render("user/index", { artists: dbres });
+  });
 });
 
 app.use("/auth", auth);
 app.use("/musician", musician);
 app.use("/user", user);
-=======
-    artistModel
-        .find()
-        .then((dbres) => {
-
-            res.render("user/index", { artists:dbres })
-
-        })
-    
-})
-
-app.use("/auth", auth)
-app.use("/musician", musician)
-app.use("/user", user)
-
-
->>>>>>> e14a85f2cbfb992f8d2dcb66e1614c86ce910de1
 
 app.listen(3000, () =>
   console.log("le serveur marche ici : http://localhost:3000")
