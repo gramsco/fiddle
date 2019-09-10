f8f9537e8a23623b76b33cad07dbdf9fc748d51c
gramsco <antoningauth@gmail.com>
2019-09-10 21:34:58 +0200

    concert page, artist page, personnal page

--- routes/concert.js
+++ routes/concert.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const router = new express.Router();
-
+const concertModel = require("../models/concert")
+const musicianModel = require("../models/musician")
 
 
 
 router.get("/:id", (req, res) => {
-
-    res.render("concerts/concert_info")
+    concertModel
+        .findById(req.params.id)
+        .then(dbconcert => {
+            musicianModel
+                .findById(dbconcert.musician)
+                .then(dbmusician => {
+                    console.log(dbmusician, dbconcert)
+                    res.render("concerts/concert_info",
+                        {
+                            concert: dbconcert,
+                            musician: dbmusician
+                        })
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 
 })
 
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,26 @@
-const express = require("express")
+const express = require("express");
 const router = new express.Router();
+const userModel = require("../models/user");
+const musicianModel = require("../models/musician");
 
 router.get("/:id", (req, res) => {
 
-    res.render("user/user")
+    userModel
+        .findById(req.params.id)
+        .then(userRes => {
+
+            musicianModel
+                .find({_id:userRes.fav_musicians})
+                .then(musiciansRes => {
+                    res.render("user/user", {
+                        user: userRes,
+                        musicians: musiciansRes
+                    })
+                })
+                .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+
 
 })
 
