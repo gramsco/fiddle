760c916c1ce636563616766aef30cb8a1513d7f2
margauxgibert15 <margaux.gibert15@hotmail.fr>
2019-09-11 11:32:09 +0200

    margaux changement

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const router = new express.Router();
+const concertModel = require("../models/concert.js");
 
-
-router.get("/", (req,res) => {
-    res.render("admin/index")
-})
-router.get("/users" , (req, res) => {
-    res.render("admin/users")
-})
+router.get("/", (req, res) => {
+  res.render("admin/index");
+});
+router.get("/users", (req, res) => {
+  res.render("admin/users");
+});
 router.get("/musicians", (req, res) => {
-    res.render("admin/musicians")
-})
-router.get("/concerts", (req, res) => {
-    res.render("admin/concerts")
-})
-router.get("/critiques", (req, res) => {
-    res.render("admin/critiques")
-})
+  res.render("admin/musicians");
+});
 
+router.get("/concerts", (req, res) => {
+  concertModel
+    .find()
+    .then(dbRes => {
+      console.log(dbRes);
+      res.render("admin/concerts", { concert: dbRes });
+    })
+    .catch(err => console.log("ça ne marche pas"));
+});
+
+router.get("/critiques", (req, res) => {
+  res.render("admin/critiques");
+});
 
 module.exports = router;
