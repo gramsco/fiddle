60a5087b968bd8ef0fcde51f9ca6f6a3b6764061
gramsco <antoningauth@gmail.com>
2019-09-12 16:51:11 +0200

    deployed

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,78 @@
 const express = require("express");
 const router = new express.Router();
+const userModel = require("../models/user");
 
 //SIGNUP
 
 router.get("/signup", (req, res) => {
-  res.render("auth/signup_choice");
-});
-
-router.get("/musician", (req, res) => {
-  res.render("auth/musician_chosen");
-});
-
-router.get("/listener", (req, res) => {
   res.render("auth/listener_chosen");
 });
 
-router.get("/signup/2", (req, res) => {
-  res.render("auth/signup_chosen");
-});
+router.post("/signup", (req, res) => {
+  
+  userModel.findOne({ username: req.body.username })
+    .then((userRes) => {
+      console.log("query went fine")
+      console.log(userRes)
+      if (userRes) {
+        console.log("already exists")
+        res.render("auth/login", { msg: "Sorry, this amazing username is already taken :(" })
+      }
+      else {
+        const { username, password, email } = req.body
+        userModel
+          .create({ username, password, email })
+          .then(() => {
+            console.log("user created!")
+            res.redirect("/")
+          })
+          .catch(err => console.log(err))
+      }
+    
+    })
+})
 
-router.get("/signup/3", (req, res) => {
-  res.render("auth/signup_final");
-});
+
 
 //CONNECT
 
 router.get("/login", (req, res) => {
   res.render("auth/login");
 });
+
+router.post("/login", (req, res) => {
+  
+  console.log(req.session)
+
+  if (!req.body.username || !req.body.password) {
+    res.render("auth/login", { msg: "Informations incomplètes" })
+  }
+  else {
+    userModel
+      .findOne({ username:req.body.username})
+      .then(userFound => {
+        if (!userFound) {
+          res.render("auth/login",
+            { msg: "Sooooorry, wrong username and/or password!" })
+        }
+        else if (req.body.password == userFound.password) {
+          req.session.currentUser = req.body
+          res.redirect("/")
+        }
+        else {
+          res.render("auth/login",
+            { msg: "Sooooorry, wrong username and/or password!" })
+        }
+      })
+      .catch(err => console.log(err))  
+  }
+});
+
+router.get("/logout", (req, res) => {
+  
+  req.session.destroy()
+  res.redirect("/")
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
 const app = express();
-
+const session = require("express-session")
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
+const MongoStore = require("connect-mongo")(session);
+
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 require("dotenv").config();
 
@@ -15,6 +17,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    cookie: { maxAge: 60000 }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
 
 //PATHS
 let auth = require("./routes/auth.js");
@@ -41,32 +57,52 @@ const critiqueModel = require("./models/critique");
 const concertModel = require("./models/concert.js");
 const userModel = require("./models/user");
 
-app.get("/", (req, res) => {
+
+      
+function checkloginStatus(req, res, next) {
+  res.locals.user = req.session.currentUser ? req.session.currentUser : null;
+  // access this value @ {{user}} or {{user.prop}} in .hbs
+  console.log(res.locals.user)
+  res.locals.isLoggedIn = Boolean(req.session.currentUser);
+  // access this value @ {{isLoggedIn}} in .hbs
+  next(); // continue to the requested route
+}
+
+function checkPrivate(req, res, next) {
+  
+  if (!req.session.currentUser) {
+    console.log("not connected??")
+    res.redirect("/auth/login")
+  }
+
+  next()
+
+}
+
+app.use(checkloginStatus)
+  
+app.get("/", checkloginStatus, (req, res) => {
   musicianModel
     .find()
     .then(artists => {
       critiqueModel
         .find()
+        .populate("concert")
         .then(critiques => {
           console.log(critiques)
-          console.log(`y'a ${critiques.length} critiques`)
           concertModel
             .find()
             .then(concerts => {
-              console.log(`y'a ${concerts.length} concerts`)
-              console.log("pfiouh all went fine, man.")
-              res.render("user/index", { artists,critiques,concerts });
+              res.render("user/index", { artists, critiques, concerts });
             })
         })
     })
 })
-      
-    
 
 app.use("/auth", auth);
 app.use("/musician", musician);
 app.use("/user", user);
-app.use("/admin", admin);
+app.use("/admin", checkPrivate, admin);
 app.use("/concert", concert);
 app.use("/musician",musician)
 app.use("/critic",critic)
